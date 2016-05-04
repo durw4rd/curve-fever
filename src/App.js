0261e4efb7788fe3cc03a07a55f221502de0e197
iulia0103 <iulia.leancu@gmail.com>
2016-05-04 09:53:53 +0200

    form - add new player

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import InfoComponent from './components/InfoComponent'
-import AppBar from 'material-ui/lib/app-bar';
+import GameModel from './models/GameModel';
+import Utils from './lib/Utils';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import Theme from './lib/Theme';
+import AppBarComponent from './components/AppBarComponent';
+import InfoComponent from './components/InfoComponent';
+import NewPlayer from './components/NewPlayer';
+import NewSnake from './components/NewSnake';
 
 class App extends React.Component {
+
+  getChildContext() {
+     return {
+       muiTheme: ThemeManager.getMuiTheme(Theme),
+     };
+   }
+
+  constructor() {
+    super();
+
+    this.games = new GameModel();
+    this.utils = new Utils();
+
+    let playerStorage = this.utils.store("curvefever.player");
+    if (playerStorage.length === 0) {
+      playerStorage = null;
+    }
+
+    this.state = {
+      games: [],
+      currentGame: null,
+      currentPlayer: playerStorage,
+    };
+  }
+
+  setPlayer(player) {
+    this.setState({
+      currentPlayer: player
+    });
+    this.utils.store("curvefever.player", player);
+  }
+
     render() {
         return (
             <div>
               <AppBar title="Curve Fever"/>
               <InfoComponent />
+              <NewPlayer onCreate={this.setPlayer.bind(this)} />
             </div>
         );
     }
